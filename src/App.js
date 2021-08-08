72b9b4b01a986616b2f18ef7d2c6ca3f408e78e8
Robert Dargavel Smith <teticio@gmail.com>
2021-08-08 11:48:58 +0100

    add my stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// TODO
+// press button twice
+// search "" => zero results
+// bootstrap theme
+// formatting
+// faiss in backend
+// highlight "waypoints"
+// screens (create, explore, settings, ...)
+// rate playlists
+// top / recent playlists
+// spotify login (existing react compoent?)
+
+import { useState } from "react";
+import { FaPlay, FaCog } from "react-icons/fa";
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Banner from './components/Banner';
+import AddTrack from './components/AddTrack';
+import Playlist from './components/Playlist';
+import RemovablePlaylist from './components/RemovablePlaylist';
+import GeneratePlaylist from './components/GeneratePlaylist';
+import playlists from "./data/playlists.json";
 
 function App() {
+  const [playlist, setPlaylist] = useState(playlists[0]);
+  const [generatedPlaylist, setGeneratedPlaylist] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <Banner />
+
+      <Card>
+        <Card.Body>
+
+          <AddTrack onAdd={(id) => {
+            setPlaylist({ 'tracks': playlist.tracks.concat({ 'id': id }) });
+          }} />
+          <hr />
+
+          <RemovablePlaylist {...playlist} onRemove={(id) => {
+            setPlaylist({ 'tracks': playlist.tracks.filter((element, index) => index !== id) });
+          }} />
+          <hr />
+
+          <div className="d-flex align-items-center justify-content-between">
+            <FaPlay onClick={() => GeneratePlaylist(playlist).then(playlist => setGeneratedPlaylist(playlist)) } />
+            <FaCog onClick={() => GeneratePlaylist(playlist).then(playlist => setGeneratedPlaylist(playlist)) } />
+          </div>
+
+        </Card.Body>
+      </Card>
+
+      <Card style={{ width: '30rem' }}>
+        <Card.Body>
+          <Playlist {...generatedPlaylist} />
+        </Card.Body>
+      </Card>
+
+    </Container>
   );
 }
 
