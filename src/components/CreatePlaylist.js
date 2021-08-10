47a71de47023ea9f62528ae3689d93103220b3de
Robert Dargavel Smith <teticio@gmail.com>
2021-08-10 08:24:23 +0100

    add spinner

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaForward, FaCog } from "react-icons/fa";
 import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
 import AddTrack from './AddTrack';
 import RemovablePlaylist from './RemovablePlaylist';
 import GeneratePlaylist from './GeneratePlaylist';
@@ -8,6 +9,7 @@ import SavePlaylist from './SavePlaylist';
 
 export default function CreatePlaylist({ onCreate = f => f }) {
   const [playlist, setPlaylist] = useState({ tracks: [] });
+  const [spinner, setSpinner] = useState(false);
 
   return (
     <Card>
@@ -23,18 +25,23 @@ export default function CreatePlaylist({ onCreate = f => f }) {
         <RemovablePlaylist {...playlist} onRemove={(id) => {
           setPlaylist({ 'tracks': playlist.tracks.filter((element, index) => index !== id) });
         }} />
-        {playlist.tracks.length > 0?
-          <hr />:
+        {playlist.tracks.length > 0 ?
+          <hr /> :
           <div />}
-        <div className="d-flex align-items-center justify-content-between">
-          <FaForward onClick={() => GeneratePlaylist(playlist)
-            .then(playlist => {
-              SavePlaylist(playlist).then(id => playlist.id = id);
-              return playlist;
-            })
-            .then(playlist => onCreate(playlist))} />
-          <FaCog onClick={() => GeneratePlaylist(playlist).then(playlist => onCreate(playlist))} />
-        </div>
+        {spinner ?
+          <Spinner animation="border" /> :
+          <div className="d-flex align-items-center justify-content-between">
+            <FaForward onClick={() => {
+              setSpinner(true);
+              GeneratePlaylist(playlist)
+                .then(playlist => {
+                  SavePlaylist(playlist).then(id => playlist.id = id);
+                  return playlist;
+                })
+                .then(playlist => onCreate(playlist))
+            }} />
+            <FaCog onClick={() => { console.log('settings'); }} />
+          </div>}
       </Card.Body>
     </Card>
   );
