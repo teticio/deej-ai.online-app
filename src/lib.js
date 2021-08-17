cc9687f472e8815ec81a8e3180faa8a6a6f13f2a
Robert Dargavel Smith <teticio@gmail.com>
2021-08-17 09:41:18 +0100

    use sessionstorage not localstorage

--- src/lib.js
+++ src/lib.js
@@ -14,10 +14,10 @@ export const debounceFunction = function (func, delay) {
 
 export const usePersistedState = (key, defaultValue) => {
   const [state, setState] = useState(
-    () => JSON.parse(localStorage.getItem(key)) || defaultValue
+    () => JSON.parse(sessionStorage.getItem(key)) || defaultValue
   );
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
+    sessionStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
   return [state, setState];
 }
