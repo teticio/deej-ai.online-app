db91c191a8ee054d021df182dcae3e93dc5b71e5
Robert Dargavel Smith <teticio@gmail.com>
2021-08-21 07:55:41 +0100

    no better than before

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,32 +1,19 @@
-import { useState, useRef, useEffect } from 'react';
+import { Suspense, useState } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import './Track.css';
 
 function SpotifyTrackWidget({ track_id, highlight, loaded, onLoad = f => f }) {
-  const ref = useRef();
-
-  useEffect(() => {
-    const iframe = ref.current.children[0];
-    const listener = () => onLoad();
-
-    iframe.addEventListener('load', listener);
-    return () => iframe.removeEventListener('load', listener);
-  }, [ref, onLoad]);
-
   return (
-    <div dangerouslySetInnerHTML={{
-      __html:
-        `<iframe src="https://open.spotify.com/embed/track/${track_id}"
-          class=${(loaded && highlight) ? "highlight" : ""}
-          title=${track_id}
-          width=${loaded ? "100%" : "0%"}
-          height="80"
-          frameBorder="0"
-          allowtransparency="true"
-          allow="encrypted-media"
-        />`
-      }}
-      ref={ref}
+    <iframe
+      className={(loaded && highlight) ? "highlight" : ""}
+      title={track_id}
+      src={`https://open.spotify.com/embed/track/${track_id}`}
+      width={loaded ? "100%" : "0%"}
+      height="80"
+      frameBorder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+      onLoad={() => onLoad()}
     />
   );
 }
@@ -35,30 +22,13 @@ export default function Track({ track_id, highlight = false, onLoad = f => f }) 
   const [loaded, setLoaded] = useState(false);
 
   return (
-    <>
-      {!loaded ?
-        <>
-          <Spinner animation="border" />
-          <SpotifyTrackWidget
-            track_id={track_id}
-            highlight={highlight}
-            loaded={loaded}
-            onLoad={() => {
-              setLoaded(true);
-              onLoad();
-            }}
-          />
-        </> :
-        <SpotifyTrackWidget
-          track_id={track_id}
-          highlight={highlight}
-          loaded={loaded}
-          onLoad={() => {
-            setLoaded(true);
-            onLoad();
-          }}
-        />
-      }
-    </>
+    <Suspense fallback={<Spinner animation="border" />} >
+      <SpotifyTrackWidget
+        track_id={track_id}
+        highlight={highlight}
+        loaded={loaded}
+        onLoad={() => { setLoaded(true); onLoad(); }}
+      />
+    </Suspense>
   );
 }
