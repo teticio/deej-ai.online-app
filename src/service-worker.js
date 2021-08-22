44134035b1d6f9f63698d8e78af1e3a9b0be881e
Robert Dargavel Smith <teticio@gmail.com>
2021-08-22 10:15:35 +0100

    don't precache but use StaleWhileRevalidate

--- src/service-worker.js
+++ src/service-worker.js
@@ -28,24 +28,8 @@ const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
-      return false;
-    } // If this is a URL that starts with /_, skip.
-
-    if (url.pathname.startsWith('/_')) {
-      return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
-
-    if (url.pathname.startsWith('/api')) {
-      return false;
-    } // Return true to signal that we want to use the handler.
-
-    return true;
+    // don't pre-cache anything
+    return false;
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
@@ -54,9 +38,9 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) => url.origin === self.location.origin, // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
-    cacheName: 'images',
+    cacheName: 'static',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
