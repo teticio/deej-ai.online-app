0b850341f0d3f3a1a0804c3a2f0a6341a0a465e0
teticio <teticio@gmail.com>
2021-08-29 16:15:36 +0100

    use build-arg in Docker

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 eval $(minikube -p minikube docker-env)
-docker build . -f helm-chart/images/Dockerfile -t teticio/deejai
+docker build . -f helm-chart/images/Dockerfile --build-arg APP_URL=${1-http://localhost:8080} -t teticio/deejai
