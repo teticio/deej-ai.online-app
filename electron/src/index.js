411ab87cfaf2933f9ba4c404c2bfde0f0a52902c
teticio <teticio@gmail.com>
2021-11-01 14:19:58 +0000

    snap

--- electron/src/index.js
+++ electron/src/index.js
@@ -21,7 +21,7 @@ function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 700,
     webPreferences: {
       nodeIntegration: true
     }
