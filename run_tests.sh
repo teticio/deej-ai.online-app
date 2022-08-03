14e81fdb5c079244ee315bc2e2f2c7ba9a63b9c4
teticio <teticio@gmail.com>
2022-08-03 13:58:16 +0100

    make test run backend and clean up dev requirements

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+APP_URL=http://localhost:3000 NO_CACHE=1 uvicorn backend.main:app --port=8001 &
+backendPID=$!
 pipenv run flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics && \
 pipenv run flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics && \
 export SQLALCHEMY_DATABASE_URL="sqlite:///:memory:" && \
@@ -7,3 +9,4 @@ export NO_CACHE='1' && \
 pipenv run pytest backend &&\
 export CI=true && \
 yarn test
+kill $backendPID
