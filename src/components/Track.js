a3b5cd03b006ed3945fa610deeffce623f151a61
Robert Smith <teticio@gmail.com>
2022-10-07 08:08:15 +0100

    update expo

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IFrame } from './Platform';
 
 export default function Track({ track_id }) {
+  const [visibility, setVisibility] = useState('hidden');
+
   return (
     <IFrame
       title={track_id}
       width='100%'
       height={80}
+      style={{
+        visibility: visibility
+      }}
+      onLoad={() => setVisibility('visible')}
       src={`${process.env.REACT_APP_API_URL}/track_widget` +
         `?track_id=${encodeURIComponent(track_id)}`
       }
