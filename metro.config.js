4684ccee109ecf15cd99c922978346d931b962ad
Robert Smith <teticio@gmail.com>
2022-10-07 18:42:09 +0100

    ignore electron in metro bundle

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+// exclusionList is a function that takes an array of regexes and combines
+// them with the default exclusions to return a single regex.
 
 module.exports = getDefaultConfig(__dirname);
+module.exports.resolver = {
+    blacklistRE: exclusionList([/electron\/.*/])
+};
