8ead5452e1440af363746fcfdcad654a84694e8e
Robert Dargavel Smith <teticio@gmail.com>
2023-06-01 08:26:32 +0100

    fix CI/CD

--- src/components/TrackSelector.test.js
+++ src/components/TrackSelector.test.js
@@ -1,13 +1,13 @@
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import TrackSelector from './TrackSelector';
 
-jest.setTimeout(40000);
+jest.setTimeout(20000);
 
 test('track search', async () => {
   render(<TrackSelector />);
   fireEvent.change(screen.getByRole('textbox'), {
     target: { value: 'james brown' },
   });
-  const tracks = await waitFor(() => screen.getAllByTestId('track'), {timeout: 40000});
+  const tracks = await waitFor(() => screen.getAllByTestId('track'), {timeout: 20000});
   expect(tracks).toMatchSnapshot();
 });
