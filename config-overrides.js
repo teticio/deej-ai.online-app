0f814e2f7480dab7aaf90cc171325c598d3208fe
Robert Dargavel Smith <teticio@gmail.com>
2023-12-24 18:41:16 +0000

    hack around undefined __DEV__ error

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,7 @@ module.exports = function override(config, env) {
 
     config.plugins.push(
         new webpack.DefinePlugin({
-            __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true'))
+            '__DEV__': JSON.stringify(process.env.NODE_ENV !== 'production')
         })
     );
 
